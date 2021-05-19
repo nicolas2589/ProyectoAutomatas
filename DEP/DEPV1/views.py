1495b37064f6cb76e870ad7361a4446d508794b2
Say10IsMe <amsuarezb@hotmail.com>
2021-05-18 18:34:02 -0700

    JOJOJO

--- DEP/DEPV1/views.py
+++ DEP/DEPV1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+
+
+class Index(template.View):
+    template_name="DEPV1/index.html"
+    login_url="DEPV1:login"

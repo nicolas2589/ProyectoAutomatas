2b06a98e4d22b3b1f16afdc84adf9f9d298feace
Say10IsMe <amsuarezb@hotmail.com>
2021-05-19 18:47:32 -0700

    jojo

--- DEP/DEPV1/urls.py
+++ DEP/DEPV1/urls.py
@@ -5,7 +5,7 @@ from DEPV1 import views
 app_name="DEPV1"
 
 urlpatterns=[
-path('login/',auth_views.LoginView.as_view(template_name="DEPV1/login.html"),name="login"),
-path('logout/',auth_views.LogoutView.as_view(template_name="DEPV1/login.html"),name="logout"),
+#path('login/',auth_views.LoginView.as_view(template_name="DEPV1/login.html"),name="login"),
+#path('logout/',auth_views.LogoutView.as_view(template_name="DEPV1/login.html"),name="logout"),/
 path('index/',views.Index.as_view(),name="index"),
 ]

cd1025bfa3134cd81b0560e47ab9fb6cd678ca79
Ariel <Ariel.landa17@tectijuana.edu.mx>
2021-06-08 21:31:04 -0700

    quitar acentos de la base

--- DEP/DEPV1/proyecto.pl
+++ DEP/DEPV1/proyecto.pl
@@ -1,144 +1,148 @@
-es_sintoma_de(tos, gripe).
-es_sintoma_de(escalofríos, gripe).
-es_sintoma_de(congestion_nasal, gripe).
-es_sintoma_de(secreción_nasal, gripe).
-es_sintoma_de(fatiga, gripe).
-es_sintoma_de(dolor_de_cabeza, gripe).
-es_sintoma_de(dolor_de_cuerpo, gripe).
-es_sintoma_de(estornudos, gripe).
+Es_sintoma_de(tos, gripe).
+Es_sintoma_de(escalofrios, gripe).
+Es_sintoma_de(congestion_nasal, gripe).
+Es_sintoma_de(secrecion_nasal, gripe).
+Es_sintoma_de(fatiga, gripe).
+Es_sintoma_de(dolor_de_cabeza, gripe).
+Es_sintoma_de(dolor_de_cuerpo, gripe).
 
 
-
-es_sintoma_de(estornudos, resfriado).
-es_sintoma_de(congestion_nasal, resfriado).
-es_sintoma_de(dolor_de_cuerpo, resfriado).
-es_sintoma_de(inflamación_nasal, resfriado).
-es_sintoma_de(enrojecimiento, resfriado).
-
-
-es_sintoma_de(dolor_de_abdomen, diarrea).
-es_sintoma_de(cólicos, diarrea).
-es_sintoma_de(flatulencias, diarrea).
-es_sintoma_de(indigestión, diarrea).
-es_sintoma_de(meteorismo, diarrea).
-es_sintoma_de(náuseas, diarrea).
-es_sintoma_de(vómitos, diarrea).
-es_sintoma_de(deshidratación, diarrea).
-es_sintoma_de(fiebre, diarrea).
-es_sintoma_de(sed, diarrea).
+Es_sintoma_de(secrecion_nasal, resfriado).
+Es_sintoma_de(estornudos, resfriado).
+Es_sintoma_de(congestion_nasal, resfriado).
+Es_sintoma_de(dolor_de_cuerpo, resfriado).
+Es_sintoma_de(inflamacion_nasal, resfriado).
+Es_sintoma_de(enrojecimiento, resfriado).
 
 
-es_sintoma_de(fiebre, covid19).
-es_sintoma_de(tos, covid19).
-es_sintoma_de(cansancio, covid19).
-es_sintoma_de(fatiga, covid19).
-es_sintoma_de(conjuntivitis, covid19).
-es_sintoma_de(dolor_de_cuerpo, covid19).
-
-es_sintoma_de(tos, bronquitis).
-es_sintoma_de(fatiga, bronquitis).
-es_sintoma_de(malestar, bronquitis).
-es_sintoma_de(congestion_nasal, bronquitis).
-es_sintoma_de(dolor_de_garganta, bronquitis).
-es_sintoma_de(dificultad_para_respirar, bronquitis).
+Es_sintoma_de(dolor_de_abdomen, diarrea).
+Es_sintoma_de(colicos, diarrea).
+Es_sintoma_de(flatulencias, diarrea).
+Es_sintoma_de(indigestion, diarrea).
+Es_sintoma_de(meteorismo, diarrea).
+Es_sintoma_de(nauseas, diarrea).
+Es_sintoma_de(vomitos, diarrea).
+Es_sintoma_de(deshidratacion, diarrea).
+Es_sintoma_de(fiebre, diarrea).
+Es_sintoma_de(sed, diarrea).
 
 
-es_sintoma_de(tos, tuberculosis).
-es_sintoma_de(dolor_de_pecho, tuberculosis).
-es_sintoma_de(escalofríos, tuberculosis).
-es_sintoma_de(fatiga, tuberculosis).
-es_sintoma_de(fiebre, tuberculosis).
-es_sintoma_de(malestar, tuberculosis).
-es_sintoma_de(sudoración, tuberculosis).
-es_sintoma_de(flemas, tuberculosis).
-es_sintoma_de(perdida_de_peso, tuberculosis).
-es_sintoma_de(dificultad_para_respirar, tuberculosis).
+Es_sintoma_de(fiebre, covid-19).
+Es_sintoma_de(tos, covid-19).
+Es_sintoma_de(cansancio, covid-19).
+Es_sintoma_de(fatiga, covid-19).
+Es_sintoma_de(conjuntivitis, covid-19).
+Es_sintoma_de(dolor_de_cuerpo, covid-19).
 
-es_sintoma_de(dolor_de_cuerpo, gripe_aviar).
-es_sintoma_de(fiebre, gripe_aviar).
-es_sintoma_de(malestar, gripe_aviar).
-es_sintoma_de(congestion_nasal, gripe_aviar).
-es_sintoma_de(estornudos, gripe_aviar).
-es_sintoma_de(dolor_de_garganta, gripe_aviar).
-es_sintoma_de(tos, gripe_aviar).
+Es_sintoma_de(tos, bronquitis).
+Es_sintoma_de(fatiga, bronquitis).
+Es_sintoma_de(malestar, bronquitis).
+Es_sintoma_de(congestion_nasal, bronquitis).
+Es_sintoma_de(dolor_de_garganta, bronquitis).
+Es_sintoma_de(dificultad_para_respirar, bronquitis).
 
 
-es_sintoma_de(fiebre, gripe_porcina).
-es_sintoma_de(tos, gripe_porcina).
-es_sintoma_de(dolor_de_garganta, gripe_porcina).
-es_sintoma_de(dolor_de_cuerpo, gripe_porcina).
-es_sintoma_de(escalofríos, gripe_porcina).
-es_sintoma_de(vomitos, gripe_porcina).
-es_sintoma_de(náuseas, gripe_porcina).
-es_sintoma_de(fatiga, gripe_porcina).
-es_sintoma_de(dolor_de_cabeza, gripe_porcina).
+Es_sintoma_de(tos, tuberculosis).
+Es_sintoma_de(dolor_de_pecho, tuberculosis).
+Es_sintoma_de(escalofrios, tuberculosis).
+Es_sintoma_de(fatiga, tuberculosis).
+Es_sintoma_de(fiebre, tuberculosis).
+Es_sintoma_de(malestar, tuberculosis).
+Es_sintoma_de(sudoracion, tuberculosis).
+Es_sintoma_de(flemas, tuberculosis).
+Es_sintoma_de(perdida_de_peso, tuberculosis).
+Es_sintoma_de(dificultad_para_respirar, tuberculosis).
+
+Es_sintoma_de(dolor_de_cuerpo, gripe_aviar).
+Es_sintoma_de(fiebre, gripe_aviar).
+Es_sintoma_de(malestar, gripe_aviar).
+Es_sintoma_de(congestion_nasal, gripe_aviar).
+Es_sintoma_de(estornudos, gripe_aviar).
+Es_sintoma_de(dolor_de_garganta, gripe_aviar).
+Es_sintoma_de(tos, gripe_aviar).
 
 
-es_sintoma_de(tos, neumonía).
-es_sintoma_de(flemas, neumonía).
-es_sintoma_de(fiebre, neumonía).
-es_sintoma_de(escalofríos, neumonía).
-es_sintoma_de(fatiga, neumonía).
-es_sintoma_de(malestar, neumonía).
-es_sintoma_de(sudoración, neumonía).
-es_sintoma_de(dificultad_para_respirar, neumonía).
+Es_sintoma_de(fiebre, gripe_porcina).
+Es_sintoma_de(tos, gripe_porcina).
+Es_sintoma_de(dolor_de_garganta, gripe_porcina).
+Es_sintoma_de(dolor_de_cuerpo, gripe_porcina).
+Es_sintoma_de(escalofrios, gripe_porcina).
+Es_sintoma_de(vomitos, gripe_porcina).
+Es_sintoma_de(nauseas, gripe_porcina).
+Es_sintoma_de(fatiga, gripe_porcina).
+Es_sintoma_de(dolor_de_cabeza, gripe_porcina).
+
+
+Es_sintoma_de(tos, neumonía).
+Es_sintoma_de(flemas, neumonía).
+Es_sintoma_de(fiebre, neumonía).
+Es_sintoma_de(escalofrios, neumonía).
+Es_sintoma_de(fatiga, neumonía).
+Es_sintoma_de(malestar, neumonía).
+Es_sintoma_de(sudoracion, neumonía).
+Es_sintoma_de(dificultad_para_respirar, neumonía).
+
 
 /* tratamientos generales */
-es_tratamiento_de(aspirina, dolor_de_cabeza).
-es_tratamiento_de(ibuprofeno, dolor_de_cabeza).
-es_tratamiento_de(paracetamol, dolor_de_cabeza).
+Es_tratamiento_de(aspirina, dolor_de_cabeza).
+Es_tratamiento_de(ibuprofeno, dolor_de_cabeza).
+Es_tratamiento_de(paracetamol, dolor_de_cabeza).
 
-es_tratamiento_de(ciprofloxacina_oftálmica, conjuntivitis).
+Es_tratamiento_de(ciprofloxacina_oftálmica, conjuntivitis).
 
-es_tratamiento_de(ibuprofeno, dolor_de_cuerpo).
-es_tratamiento_de(naproxeno, dolor_de_cuerpo).
+Es_tratamiento_de(ibuprofeno, dolor_de_cuerpo).
+Es_tratamiento_de(naproxeno, dolor_de_cuerpo).
 
-es_tratamiento_de(paracetamol, fiebre).
-es_tratamiento_de(ibuprofeno, fiebre).
+Es_tratamiento_de(paracetamol, fiebre).
+Es_tratamiento_de(ibuprofeno, fiebre).
 
-es_tratamiento_de(antitusígenos, tos).
-es_tratamiento_de(antihistamínicos, secreción_nasal).
+Es_tratamiento_de(antitusígenos, tos).
+Es_tratamiento_de(antihistamínicos, secreción_nasal).
 
-es_tratamiento_de(sro, vómitos).
+Es_tratamiento_de(sro, vomitos).
 
-es_tratamiento_de(paracetamol, escalofríos).
+Es_tratamiento_de(paracetamol, escalofrios).
 
 
 /* tratamientos para enfermedades */
-es_tratamiento_de(descongestivo, gripe).
-es_tratamiento_de(antitusígeno, gripe).
-es_tratamiento_de(antiinflamatorio, gripe).
-es_tratamiento_de(analgésico ,gripe).
-es_tratamiento_de(antiviral, gripe).
+
+Es_tratamiento_de(descongestivo, gripe).
+Es_tratamiento_de(antitusígeno, gripe).
+Es_tratamiento_de(antiinflamatorio, gripe).
+Es_tratamiento_de(analgésico ,gripe).
+Es_tratamiento_de(antiviral, gripe).
 
 
-es_tratamiento_de(analgésicos, resfriado).
-es_tratamiento_de(antihistamínico, resfriado).
-es_tratamiento_de(antiinflamatorio, resfriado).
-es_tratamiento_de(antitusígeno, resfriado).
-es_tratamiento_de(descongestivo, resfriado).
-es_tratamiento_de(sro, diarrea).
-es_tratamiento_de(bismuto, diarrea).
-es_tratamiento_de(loperamida, diarrea).
+Es_tratamiento_de(analgésicos, resfriado).
+Es_tratamiento_de(antihistamínico, resfriado).
+Es_tratamiento_de(antiinflamatorio, resfriado).
+Es_tratamiento_de(antitusígeno, resfriado).
+Es_tratamiento_de(descongestivo, resfriado).
 
-es_tratamiento_de(vacuna, covid19).
-es_tratamiento_de(analgésico, bronquitis).
-es_tratamiento_de(antiinflamatorio, bronquitis).
-es_tratamiento_de(narcótico, bronquitis).
-es_tratamiento_de(antitusígeno, bronquitis).
+Es_tratamiento_de(sro, diarrea).
+Es_tratamiento_de(bismuto, diarrea).
+Es_tratamiento_de(loperamida, diarrea).
 
-es_tratamiento_de(antibiótico, tuberculosis).
-es_tratamiento_de(antiviral, gripe_aviar).
-es_tratamiento_de(oseltamivir, gripe_aviar).
-es_tratamiento_de(zanamivir, gripe_aviar).
+Es_tratamiento_de(vacuna, covid-19).
 
-es_tratamiento_de(antiviral, gripe_porcina).
-es_tratamiento_de(antitusígeno, gripe_porcina).
-es_tratamiento_de(descongestivo, gripe_porcina).
-es_tratamiento_de(analgésico, gripe_porcina).
-es_tratamiento_de(antiinflamatorio, gripe_porcina).
+Es_tratamiento_de(analgésico, bronquitis).
+Es_tratamiento_de(antiinflamatorio, bronquitis).
+Es_tratamiento_de(narcótico, bronquitis).
+Es_tratamiento_de(antitusígeno, bronquitis).
 
-es_tratamiento_de(antibiótico, neumonía).
-es_tratamiento_de(penicilina, neumonía).
+Es_tratamiento_de(antibiótico, tuberculosis).
+
+Es_tratamiento_de(antiviral, gripe_aviar).
+Es_tratamiento_de(oseltamivir, gripe_aviar).
+Es_tratamiento_de(zanamivir, gripe_aviar).
+
+Es_tratamiento_de(antiviral, gripe_porcina).
+Es_tratamiento_de(antitusígeno, gripe_porcina).
+Es_tratamiento_de(descongestivo, gripe_porcina).
+Es_tratamiento_de(analgésico, gripe_porcina).
+Es_tratamiento_de(antiinflamatorio, gripe_porcina).
+
+Es_tratamiento_de(antibiótico, neumonía).
+Es_tratamiento_de(penicilina, neumonía).
 
 es_tratamiento_de(Y,X):-es_sintoma_de(Z,X), es_tratamiento_de(Y,Z).
